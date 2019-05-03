6efef913712c99ed32aa1244afd2aab64f0e2541
Vedran Vugrin <vedran.vugrin@clover-studio.com>
2019-05-03 16:30:53 +0200

    Added a few notification categories and actions.

--- CSPushManager/CSPushManager.swift
+++ CSPushManager/CSPushManager.swift
@@ -13,6 +13,10 @@ import UserNotifications
 
 class CSPushManager: NSObject {
     
+    let kTxtMessageCategory = "txt_message_category"
+    let kShareImage = "txt_share_image"
+    let kShareUrl = "txt_share_url"
+    
     public static let instance = CSPushManager()
     
     let bag = DisposeBag()
@@ -21,13 +25,12 @@ class CSPushManager: NSObject {
         print("Starting")
         UNUserNotificationCenter.current().delegate = self
         
-        let authTrigger: Single<Bool> = authorizationTrigger
-        
-        authTrigger
+        authorizationTrigger
             .filter{ $0 }
             .observeOn(MainScheduler.instance)
             .subscribe(onSuccess: { _ in
                 application.registerForRemoteNotifications()
+                
             })
         .disposed(by: bag)
     }
@@ -37,7 +40,7 @@ class CSPushManager: NSObject {
         print("APNs device token: \(deviceTokenString)")
     }
     
-    var authorizationTrigger: Single<Bool> {
+    lazy var authorizationTrigger: Single<Bool> = {
         return Single.create(subscribe: { observer in
             UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound],
                                                                     completionHandler: { granted, error in
@@ -50,6 +53,43 @@ class CSPushManager: NSObject {
             
             return Disposables.create()
         })
+    }()
+    
+    var textMessageCategory: UNNotificationCategory {
+        return UNNotificationCategory(identifier: kTxtMessageCategory,
+                                      actions: [replyAction, openInAppAction, dismissAction],
+                                      intentIdentifiers: [],
+                                      options: [])
+    }
+    
+    var imageShareCategory: UNNotificationCategory {
+        return UNNotificationCategory(identifier: kShareImage,
+                                      actions: [replyAction, openInAppAction],
+                                      intentIdentifiers: [],
+                                      options: [])
+    }
+    
+    var urlShareCategory: UNNotificationCategory {
+        return UNNotificationCategory(identifier: kShareUrl,
+                                      actions: [replyAction, openUrlAction, openInAppAction],
+                                      intentIdentifiers: [],
+                                      options: [])
+    }
+    
+    var replyAction: UNNotificationAction {
+        return UNTextInputNotificationAction(identifier: "REPLY", title: "Quick Reply", options: [])
+    }
+    
+    var openInAppAction: UNNotificationAction {
+        return UNNotificationAction(identifier: "VIEW", title: "View", options: [.foreground])
+    }
+    
+    var dismissAction: UNNotificationAction {
+        return UNNotificationAction(identifier: "DISMISS", title: "Dismiss", options: [.destructive])
+    }
+    
+    var openUrlAction: UNNotificationAction {
+        return UNNotificationAction(identifier: "OPEN_URL", title: "Open Url", options: [.destructive])
     }
     
 }
